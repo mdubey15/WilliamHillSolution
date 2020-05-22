aa51b98a959714eaf5630c1cb21176c02125c4d2
mdubey15 <mahendra.dubey21@live.com>
2020-05-22 19:48:34 +0200

    first committ

---
+++ WilliamHillSolution/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using FactoryCustomer;
+using WilliamHillSolution.Controllers;
+
+namespace WilliamHillSolution
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddDbContext<DbContext>(op =>
+            {
+                op.UseSqlServer(Configuration.GetConnectionString("Database"));
+            }); //Add
+            services.AddCors();
+            services.AddScoped<ILogger, Logger<CustomerController>>();
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(
+                options => options.AllowAnyOrigin()
+                .AllowAnyMethod()
+            );
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
